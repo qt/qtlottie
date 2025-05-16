dea308fc93aad7a437aea7e228e9bfae18ce86f1
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-16 19:32:54 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json accordingly.
    
    Task-number: QTBUG-136484
    Change-Id: Ie0881ba2ca7afae0f94ca153ae66f80814ee261d
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/bodymovin/bmfreeformshape_p.h
+++ src/bodymovin/bmfreeformshape_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef BMFEEFORMSHAPE_P_H
 #define BMFEEFORMSHAPE_P_H
