806632d84f167a45e82abead10c5a384d5809250
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-09-10 12:26:24 +0300

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit dea308fc93aad7a437aea7e228e9bfae18ce86f1.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial SPDX license identifiers.
    
    Task-number: QTLTSOSS-264
    Change-Id: Id1438ce3edb4b2e3d4352bd0b9cf684ae9fefe2f
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/bodymovin/bmimagelayer_p.h
+++ src/bodymovin/bmimagelayer_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #ifndef BMIMAGELAYER_P_H
 #define BMIMAGELAYER_P_H
--- src/bodymovin/bmspatialproperty_p.h
+++ src/bodymovin/bmspatialproperty_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #ifndef BMSPATIALPROPERTY_P_H
 #define BMSPATIALPROPERTY_P_H
